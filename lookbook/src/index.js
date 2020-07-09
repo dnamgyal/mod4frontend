9e2bdbb06174d7dffa62eeb907a3167205533470
rwilt <rosie.wilt@gmail.com>
2020-07-09 14:55:44 -0400

    changes

--- lookbook/src/index.js
+++ lookbook/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
