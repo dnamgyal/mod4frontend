5de144ac3599fef490e0a91f40c80a66b1bae768
rwilt <rosie.wilt@gmail.com>
2020-07-10 00:43:19 -0400

    styling

--- lookbook/src/Components/Mannequin.js
+++ lookbook/src/Components/Mannequin.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import '../App.css';
 import man from './man1.png'
+import woman from './girl-model-1.png'
 import Form from './Form'
 
 class Mannequin extends React.Component {
 
+  state = {
+    choice: true
+  }
+
+  handleClick = () => {
+    this.setState((prevState) =>{
+    return {choice: !prevState.choice}
+    })
+  }
 
  render () {
 
       return (
         <React.Fragment>
-        <img src={man} onClick = {this.handleClick} id="male-model" alt="man in underwear"/>
+          <button onClick={this.handleClick}>change model</button>
+        {this.state.choice ? <img src={woman} onClick = {this.handleClick} id="male-model" alt="woman in underwear"/> :
+          <img src={man} onClick = {this.handleClick} id="male-model" alt="man in underwear"/>
+        }
+      
+        
         <br></br>
         <br></br>
        <Form
